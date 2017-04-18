213c7b9757f135dd7327bc593e0c9f8822cdce81
grb <473602668@qq.com>
2017-04-18 20:34:04 +0800

    modify 11_bounced2d.c

--- 7-game/11_bounce2d.c
+++ 7-game/11_bounce2d.c
@@ -1,5 +1,4 @@
 /* 	brief  ：实现功能 => 让求在上下左右跳动
- *  	note   :
  *  		实现原理:
 		usge:
 				user input: 	
@@ -10,17 +9,27 @@
  *		 	 	        Q quit
  *  	histroy:			
  *  		2016-04-16	renbin.guo created
+ *  		2016-04-18	renbin.guo added command 'note'
+ *  			
+ *  	note   :	
+ *  		bug:	疑问：如果我们按键，把y_ttm减为负数了怎么办?貌似是个bug
+ *  	
+ *  		gcc 11_bounce2d.c set_ticker.c -lcurses -o bounce2d 
+ *  		
  *  			
  ****************************************/
 
 #include<stdio.h>
 #include<signal.h>
-#include"bounce.h"	//头文件定义在哪里
+#include<ncurses.h>
+#include<sys/time.h>
+#include"bounce.h"	
 
 struct	ppball	the_ball;
 
 void  set_up();
 void  wrap_up();
+void ball_move(int signum);
 
 int main()
 {
@@ -93,6 +102,22 @@ void ball_move(int signum)
 	x_cur = the_ball.x_pos;
 	moved = 0;
 	
+	/* 
+ 	 *  我们来考察什么都不按的情况:
+ 	 *	初始时，y_ttg=y_ttm=8,显然满足y_ttm>0,但是不满足y_ttg--==1,所以不进入执行，但是y_ttg变为了7了
+ 	 *	然后一直减，知道y_ttg为2的时候，OK，可以进去执行了，此时图形就可以运动了，同时，y_ttg被赋新值y_ttm = 8
+ 	 *  再考虑我们按键的情况：
+ 	 *  	当我们按下'S'时，y_ttm++，变为9，所以下次为y_ttg赋值就为了9，所以要运动，时间就会花的长一点,从而可以控制.
+ 	 *  	速度.
+ 	 *  疑问：如果我们按键，把y_ttm减为负数了怎么办?貌似是个bug
+ 	 *
+ 	 */
+	if ( the_ball.y_ttm > 0 && the_ball.y_ttg-- == 1 ){
+		the_ball.y_pos += the_ball.y_dir ;	/* move	*/      // y轴位置是否移动，要看y_ttm变量是否大于0
+		the_ball.y_ttg = the_ball.y_ttm  ;	/* reset*/      // 还要看y_ttg是否大于1
+		moved = 1;                                          // 如果都满足，则y_pos会增加y_dir.
+	}
+		                                                    //
 	/* 这里只对x方向设置 */
 	if(the_ball.x_ttm > 0 && the_ball.x_ttg-- == 1)
 	{
@@ -100,9 +125,10 @@ void ball_move(int signum)
 		the_ball.x_ttg +=  the_ball.x_ttm; /** reset */
 		moved = 1;
 	}
+	
 	if(moved)
 	{
-		mvaddch(y_cur,x_cur,BLANK);
+		mvaddch(y_cur,x_cur,BLANK);	// 这里为什么要两次?
 		mvaddch(y_cur,x_cur,BLANK);
 		mvaddch(the_ball.y_pos,the_ball.x_pos,the_ball.symbol);
 		bounce_or_lose(&the_ball);
