df562273f08e399f2e01c4bf4f5c01e8b6da1ca8
grb <473602668@qq.com>
2017-04-27 07:09:51 +0800

    modify smsh1.c splitline.c

--- 8_shell/shell3/splitline.c
+++ 8_shell/shell3/splitline.c
@@ -1,9 +1,14 @@
 /******************************************
  * 	brief:	get next command
+ * 		这个分明就是把文件fd中的数据拷贝到buf中啊，哪里是get next command ?
  * 	param:	
  * 		[in]  char*prompt: 输入的提示符，这里为"> "
  * 		[in]  FILE *fp   : fd文件，存放命令字符
- */
+ * 	returns: 
+ * 		NULL   -> 文件为空
+ * 		char * -> 存放拷贝的文件的buf的地址(在堆中分配)
+ * 			
+ ********************************************************************/
 char * next_cmd(char*prompt,FILE *fp)
 {
 	char *buf;
@@ -27,7 +32,8 @@ char * next_cmd(char*prompt,FILE *fp)
 		// 如果输入的是换行符,代表执行当前行
 		if(c == '\n')
 			break;
-		buf[pos++] = c;	 //将输入的字符存在buf中
+		//将输入的字符存在buf中
+		buf[pos++] = c;	 
 	}
 	// 如果fd中什么内容也没有,即第一个字符为EOF,这种情况返回NULL
 	if(c == EOF && pos == 0)
@@ -36,7 +42,80 @@ char * next_cmd(char*prompt,FILE *fp)
 	return buf;
 }
 	
+/* 
+ *  brief : 动态分配n字节空间 (包含了出错处理的malloc)
+ */
+void *emalloc(size_t n)
+{
+	void *rv;
+	if((rv = malloc(n)) == NULL)
+		fatal("out of memory","",1);
+	return rv;
+}
 		
+
+/*
+ *  brief : 将p作为其实地址，分配n字节的空间 (包含了错误处理的realloc)
+ */
+void*erealloc(void *p,size_t n)
+{
+	void *rv;
+	if( (rv = realloc(p,n)) ==  NULL)
+		fatal("readlloc() failed","",1);
+	return rv;
+}
+
+/*
+ * 将字符串line解析为命令行参数,存放在指针数组里返回。
+ */
+char** splitlien(char *line)
+{
+	char 	*newstr();
+	char 	**args;		// 指针数组，存放解析后的参数
+	int	spots = 0;
+	int 	bufspace = 0;
+	int 	argnum = 0;	// 保存参数个数
+	char 	*cp = line;
+	char 	*start;
+	int 	len;
+	
+	if(line == NULL)
+		return NULL;
+	args	 = emalloc(BUFSIZ);	// 这里 emalloc的是一个512字节的连续空间
+	bufspace = BUFSIZ;
+	spots	 = BUFSIZ/sizeof(char *);	// BUFSIZ这么多字节，能存放的指针变量数。(BUFSIZ为512,一个指针4字节，所以能存
+						// 103个指针)
+	
+	while(*cp != '\0')
+	{
+		while(is_delim(*cp))	// 跳过开头的空格
+			cp++;
+		if(*cp == '\0')
+			break;
+		// 空间不够,参数大于了spots(103),一般是不可能的。
+		if(argnum+1 >= spots)
+		{
+			// 
+			args = erealloc(args,bufspace+BUFSIZ);
+		
+		}
+		
+		start = cp;
+		len = 1; 
+		// 现在cp不为空格,循环，直到遇到空格
+		while(*++cp != '\0' && !(is_delim(*cp)))
+			len ++;
+		args[argnum++] = nwestr(start,len);	// newstr中会malloc len长度的空间，然后把地址返回给指针args[argnum+]
+	}
+	args[argnum] = NULL;
+	return args;
+}
+
+
+	
+
+	
+	
 		
 			
 	
