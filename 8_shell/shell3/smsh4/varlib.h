86ef203d85967c60ec261ad515fdf1d6dbeaf97b
renbin.guo <renbin.guo@sim.com>
2017-04-29 16:24:11 +0800

    add smsh4

---
+++ 8_shell/shell3/smsh4/varlib.h
@@ -0,0 +1,18 @@
+
+
+int	VLexport(char *);
+void	VLlist();
+int	VLstore( char *, char * );
+
+// renbin.guo added for version 4 begin
+int	VLenviron2table(char **);
+int	VLenviron2table(char **);
+// renbin.guo added for version 4 end 
+
+struct var
+{
+        int global;
+        char *str;
+};
+
+
