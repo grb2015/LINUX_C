874fd4754beb1ef3a5841a7a7ad1d3b1d6f6cb57
grb <949409706@qq.com>
2017-07-05 16:45:02 +0800

    add 12/ exe 2

--- 14_thread/2_hello_multi.c
+++ 14_thread/2_hello_multi.c
@@ -3,6 +3,23 @@
 *               使得执行时间变为一半
 *       history ： 2017-05-09 renbin.guo created
 *
+*
+*       usage:
+*               [root@localhost 14_thread]# gcc 2_hello_multi.c -o 2_hello_multi -lpthread
+*               [root@localhost 14_thread]# ./2_hello_multi
+*               world
+*               hello
+*               world
+*               hello
+*               world
+*               hello
+*               world
+*               hello
+*               world
+*               hello
+*               [root@localhost 14_thread]# ./2_hello_multi
+*
+*
 *********************************************/
 
 /* hello_multi.c - a multi-threaded hello world program */
@@ -18,7 +35,7 @@ main()
 
 	void	*print_msg(void *);
 
-	pthread_create(&t1, NULL, print_msg, (void *)"hello");
+	pthread_create(&t1, NULL, print_msg, (void *)"hello\n");
 	pthread_create(&t2, NULL, print_msg, (void *)"world\n");
 
 	// main线程等待t1,t2结束
