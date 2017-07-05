5509bf4a69987b8bc5572aa4b73309e043f4d279
grb <949409706@qq.com>
2017-07-05 16:41:30 +0800

    modified 14/1.c and add exe 1

--- 14_thread/1_hello_single.c
+++ 14_thread/1_hello_single.c
@@ -3,6 +3,27 @@
 // 线程的引入
 // 注：执行这个程序需要10s
 // 2017-05-09 renbin.guo created  
+/**< 
+ *
+ *  uage:
+ *      [root@localhost 14_thread]# ./1_hello_single
+ *      hello
+ *      hello
+ *      hello
+ *      hello
+ *      hello
+ *      world
+ *      world
+ *      world
+ *      world
+ *      world
+ *      [root@localhost 14_thread]# 
+ *
+ *          
+ *
+ *
+ *
+ * */
 #include  <stdio.h>
 #define	NUM	5
 
@@ -10,7 +31,7 @@ main()
 {
 	void	print_msg(char *);
 
-	print_msg("hello");
+	print_msg("hello\n");
 	print_msg("world\n");
 }
 void print_msg(char *m)
